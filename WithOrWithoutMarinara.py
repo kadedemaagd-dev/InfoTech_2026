f03ad02430b7b7bbd372ddab419b9d7309526519
Demaagd <kadedemaagd@gmail.com>
2026-01-14 12:22:21 -0500

    While Loop ... 95% Stable - Doing Research

--- WithOrWithoutMarinara.py
+++ WithOrWithoutMarinara.py
@@ -1,5 +1,26 @@
 #Welcome Branch 
 
+#Libraries Imported Here 
+import sys 
+import time
+
 print("\nWelcome Branch - Developer: Kade DeMaagd")    
 print("\nWelcome to InfoTechCenter V.1.0")
-print("\nInfoTechCenter is Booting Up...")
+
+
+x = 0 
+ellipsis = 0
+
+while x != 20: 
+    x += 1
+    ellipsisMessage = ("InfoTechCenter OS Booting" + "." * ellipsis)
+    ellipsis += 1 
+    sys.stdout.write("\r" + ellipsisMessage + "   ")
+    sys.stdout.flush()
+    time.sleep(.5)
+    if ellipsis == 4:
+        ellipsis = 0 
+    if x == 20: 
+        print("\nOperating System Booted Up - Retina Scanned - Access Granted")
+
+
