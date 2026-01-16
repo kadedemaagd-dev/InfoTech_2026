7fd3fc35be6fff9a81da0246a12de37726ab955a
Kade Demaagd <kadedemaagd@gmail.com>
2026-01-16 13:34:09 -0500

    Added color - Thanks Ai

--- WithOrWithoutMarinara.py
+++ WithOrWithoutMarinara.py
@@ -1,26 +1,44 @@
-#Welcome Branch 
+# Welcome Branch
 
-#Libraries Imported Here 
-import sys 
+# Libraries Imported Here
+import sys
 import time
 
-print("\nWelcome Branch - Developer: Kade DeMaagd")    
-print("\nWelcome to InfoTechCenter V.1.0")
+# ANSI color codes
+CYAN = "\033[36m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BRIGHT = "\033[1m"
+RESET = "\033[0m"
 
+print(CYAN + "\nWelcome Branch - Developer: Kade DeMaagd" + RESET)
+print(GREEN + "\nWelcome to InfoTechCenter V.1.0" + RESET)
 
-x = 0 
+x = 0
 ellipsis = 0
 
-while x != 20: 
+while x != 20:
     x += 1
-    ellipsisMessage = ("InfoTechCenter OS Booting" + "." * ellipsis)
-    ellipsis += 1 
+
+    ellipsisMessage = (
+        YELLOW
+        + "InfoTechCenter OS Booting"
+        + "." * ellipsis
+        + RESET
+    )
+
+    ellipsis += 1
     sys.stdout.write("\r" + ellipsisMessage + "   ")
     sys.stdout.flush()
     time.sleep(.5)
+
     if ellipsis == 4:
-        ellipsis = 0 
-    if x == 20: 
-        print("\nOperating System Booted Up - Retina Scanned - Access Granted")
+        ellipsis = 0
 
-
+    if x == 20:
+        print(
+            GREEN
+            + BRIGHT
+            + "\nOperating System Booted Up - Retina Scanned - Access Granted"
+            + RESET
+        )
